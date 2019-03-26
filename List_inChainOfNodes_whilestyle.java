59fe7ca57b6f7e3c3218533a02c29e8c4e9b71e6
Ian Fried <ifried00@stuy.edu>
2019-03-26 11:23:43 -0400

    added updates for v1

--- List_inChainOfNodes_whilestyle.java
+++ List_inChainOfNodes_whilestyle.java
@@ -17,7 +17,37 @@ public class List_inChainOfNodes_whilestyle{
     return retString + "]";}
     /**Append @value to the head of this list. @return true, in keeping with conventions yet to be discussed*/
   public boolean addAsHead( Object val) {
-    Node toAddNode = new Node(val, hRef.getRefToNextNode());
-    hRef.setRefToNextNode(toAddNode);
-    return true;}
+    Node toAddNode = new Node(val, headReference.getReferenceToNextNode());
+    headReference.setReferenceToNextNode(toAddNode);
+    return true;
+  }
+  
+//CREDIT TO KYLE EDWARDS WHO DID THE CODE BELOW WITH ME IN CLASS ON 3/25/19
+  public Node get(int element){
+    Node currentNode = headReference.getReferenceToNextNode();
+    for(int x = 0; x < element; x++){
+      currentNode = currentNode.getReferenceToNextNode();
+    }
+
+    return currentNode;
+  }
+
+  public void set(Object val, int position){
+    Node currentNode = new Node(val, get(position).getReferenceToNextNode());
+    get(position-1).setReferenceToNextNode(currentNode);
+  }
+
+  public boolean add(Object val, int position){
+    Node currentNode = get(position);
+    Node newNode = new Node(val, currentNode.getReferenceToNextNode());
+    currentNode.setReferenceToNextNode(newNode);
+    return true;
+  }
+
+  public void remove(int position){
+    Node prevNode = ( (position == 0) ? headReference : get(position-1));
+    Node newNode = get(position);
+
+    prevNode.setReferenceToNextNode(newNode);
+  }
 }
